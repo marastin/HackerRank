9d5fbe3a750044d8cd3a340cf3e0e330e4fb8c87
marastin <58289836+marastin@users.noreply.github.com>
2024-02-15 14:18:28 +0100

    example 2 word order added

--- Python Examples/collections_ordereddict.py
+++ Python Examples/collections_ordereddict.py
@@ -1,6 +1,8 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 from collections import OrderedDict
 
+
+# Example 1 - HackerRank
 # Syntax 1
 N = int(input())
 OD = OrderedDict()
@@ -24,3 +26,17 @@ for _ in range(int(input())):
     OD[name] = OD.get(name, 0) + int(net_price)
 for item in OD.items():
     print(*item)
+
+
+
+# Example 2 - HackerRank Word Order
+
+OD = OrderedDict()
+for _ in range(int(input())):
+    word = input()
+    OD[word] = OD.get(word, 0) + 1
+
+print(len(OD))
+
+for item in OD.items():
+    print(item[1], end=' ')
