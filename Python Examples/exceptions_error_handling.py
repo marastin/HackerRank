65f16b14a15dfdbe3886f74bc15ae68b7880dcd4
marastin <58289836+marastin@users.noreply.github.com>
2024-02-20 13:42:45 +0100

    first commit

---
+++ Python Examples/exceptions_error_handling.py
@@ -0,0 +1,20 @@
+
+# Doc:
+# https://docs.python.org/2/library/exceptions.html#module-exceptions
+
+# Example 1
+try:
+    print(1/0)
+except ZeroDivisionError as e:
+    print("Error Code:", e)
+
+
+
+# HackerRank
+T = int(input())
+for _ in range(T):
+    try:
+        a, b = map(int, input().split())
+        print(a//b)
+    except Exception as e:
+        print("Error Code:", e)
