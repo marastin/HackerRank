419aad1f9424fd668cebf0ea21ed58f0279c0739
marastin <58289836+marastin@users.noreply.github.com>
2024-02-28 18:01:58 +0100

    add note

--- Python Examples/re_split.py
+++ Python Examples/re_split.py
@@ -9,3 +9,12 @@ re.split(r"-","+91-011-2711-1111")
 text = '100,000,000.000'
 regex_pattern = r"[.,]"	# Do not delete 'r'.
 print("\n".join(re.split(regex_pattern, text)))
+
+
+
+# Note:
+# The r prefix before a string literal denotes a raw string literal.
+# When you prefix a string literal with r (e.g., r"string"), it indicates that backslashes 
+# within the string should be treated as literal characters rather than as escape characters.
+# print("C:\path\to\file")  # Output: C:\path\to\file (incorrect)
+# print(r"C:\path\to\file") # Output: C:\path\to\file (correct)
