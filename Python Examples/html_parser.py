438a7c352b7dd0a2c82c671835b381c7f66dcecd
marastin <58289836+marastin@users.noreply.github.com>
2024-03-08 14:39:05 +0100

    add example

--- Python Examples/html_parser.py
+++ Python Examples/html_parser.py
@@ -16,10 +16,28 @@ class MyHTMLParser(HTMLParser):
         print(f"Empty : {tag}")
         for attr in attrs:
             print(f"-> {attr[0]} > {attr[1] if attr[1] else None}")
+    
+    def handle_comment(self, data):
+        data = data.split('\n')
+        if len(data) == 1:
+            print(">>> Single-line Comment")
+            print(data[0])
+        elif len(data) > 1:
+            print(">>> Multi-line Comment")
+            print("\n".join(data))
+    
+    def handle_data(self, data):
+        if data != "\n":
+            print(">>> Data")
+            print(data)
+  
 
+html = ""       
+for i in range(int(input())):
+    html += input().rstrip()
+    html += '\n'
+    
 parser = MyHTMLParser()
-N = int(input())
-for _ in range(N):
-    S = input()
-    parser.feed(S)
+parser.feed(html)
+parser.close()
     
