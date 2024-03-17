6704b49dfc3b0772b670ea8c2e5334e9ed1a658e
marastin <58289836+marastin@users.noreply.github.com>
2024-03-17 16:46:59 +0100

    add example

--- Python Examples/xml_example.py
+++ Python Examples/xml_example.py
@@ -1,11 +1,18 @@
 import sys
 import xml.etree.ElementTree as etree
 
-
+xml_sample = """
+<feed xml:lang='en'>
+    <title>HackerRank</title>
+    <subtitle lang='en'>Programming challenges</subtitle>
+    <link rel='alternate' type='text/html' href='http://hackerrank.com/'/>
+    <updated>2013-12-25T12:00:00</updated>
+</feed>
+"""
 
 # Example 1
+# Output a single line, the number of attributes in the given XML document.
 def get_attr_number(node):
-    # your code goes here
     num = 0
     for element in node.iter():
         num += len(element.attrib)
@@ -17,3 +24,24 @@ if __name__ == '__main__':
     tree = etree.ElementTree(etree.fromstring(xml))
     root = tree.getroot()
     print(get_attr_number(root))
+
+
+
+# Example 2
+# Output a single line, the integer value of the maximum level of nesting in the XML document.
+maxdepth = 0
+def depth(elem, level):
+    global maxdepth
+    level += 1
+    maxdepth = max(maxdepth, level)
+    for child in elem:
+        depth(child, level)
+
+if __name__ == '__main__':
+    n = int(input())
+    xml = ""
+    for i in range(n):
+        xml =  xml + input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(xml))
+    depth(tree.getroot(), -1)
+    print(maxdepth)
