5850664ebbdfe3fd48b13edabc2976887de13210
marastin <58289836+marastin@users.noreply.github.com>
2024-03-25 01:48:27 +0100

    add example

--- Python Examples/numpy_example.py
+++ Python Examples/numpy_example.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 # Example 1
+print('\n---Example 1---\n')
 def arrays(arr):
     return np.array(arr[::-1], dtype=float)
 
@@ -11,9 +12,21 @@ print(result)
 
 
 # Example 2
+print('\n---Example 2---\n')
 arr = np.array([1,2,3,4,5,6])
 # Using shape to get array dimensions
 print(arr.shape)
 # Using shape to change array dimensions
 arr.shape = (3, 2)
 print(arr)
+
+
+# Example 3
+# The reshape tool gives a new shape to an array without changing its data. 
+# It creates a new array and does not modify the original array itself.
+print('\n---Example 3---\n')
+my_array = np.array([1,2,3,4,5,6])
+print(np.reshape(my_array,(3,2)), '\n')
+print(my_array, '\n')
+print(my_array.reshape(2,3), '\n')
+print(my_array, '\n')
